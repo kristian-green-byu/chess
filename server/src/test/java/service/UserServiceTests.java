e57fa913f378b1717b92f6ef2e36fd421c816bcd
Kristian Green <kristiangreen@Kristians-MacBook-Air.local>
2024-07-20 23:42:55 -0600

    registerFailure local test completed (not passoff test)

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,22 +1,30 @@
 package service;
-
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import dataaccess.MemoryAuthDAO;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import requests.RegisterRequest;
 import responses.RegisterResponse;
-import service.UserService;
 
 public class UserServiceTests {
+    AuthDAO authDAO = new MemoryAuthDAO();
+    UserService userService = new UserService(authDAO);
+
     @Test
     public void registerSuccess() throws DataAccessException {
         RegisterRequest registerRequest = new RegisterRequest("username", "password", "test@email.com");
-        AuthDAO authDAO = new MemoryAuthDAO();
-        UserService userService = new UserService(authDAO);
         RegisterResponse observedResponse = userService.register(registerRequest);
         String authToken = authDAO.getAuthToken("username");
         RegisterResponse expectedResponse = new RegisterResponse("username", authToken);
         assert observedResponse.equals(expectedResponse);
+    }
+    @Test
+    public void registerFailure() throws DataAccessException {
+        RegisterRequest registerRequest = new RegisterRequest("username", null, "test@email.com");
+        try{
+            userService.register(registerRequest);
+        } catch (DataAccessException e) {
+        assert true;}
     }
 }
