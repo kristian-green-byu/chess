d315034e2cebaf794b6441073cec7f10b45b7a7b
Kristian Green <kristiangreen@Kristians-Air.app.byu.edu>
2024-07-29 15:38:43 -0600

    Finished SQLUserDAO and tests

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,6 +1,12 @@
 package dataaccess;
 
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class SQLUserDAO implements UserDAO{
 
@@ -10,13 +16,46 @@ public class SQLUserDAO implements UserDAO{
         this.databaseManager = new DatabaseManager();
     }
 
-    public UserData getUser(String username){
+    public UserData getUser(String username) throws DataAccessException {
+        if(username==null){
+            throw new DataAccessException("unauthorized");
+        }
+        Collection<UserData> users = new ArrayList<>();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = databaseManager.setDB("SELECT username, password, email FROM %DB_NAME%.userData");
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        users.add(readUserData(rs));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        for (UserData user : users) {
+            if (user.username().equals(username)) {
+                return user;
+            }
+        }
         return null;
     }
 
+    private UserData readUserData(ResultSet rs) throws SQLException {
+        var username = rs.getString("username");
+        var password = rs.getString("password");
+        var email = rs.getString("email");
+
+        return new UserData(username, password, email);
+    }
+
     public void createUser(UserData user) throws DataAccessException{
+        if(getUser(user.username())!=null){
+            throw new DataAccessException("already taken");
+        }
         var statement = databaseManager.setDB("INSERT INTO %DB_NAME%.userData (username, password, email) VALUES (?, ?, ?)");
-        databaseManager.executeUpdate(statement, user.username(), user.password(), user.email());
+        String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        databaseManager.executeUpdate(statement, user.username(), hashedPassword, user.email());
     }
 
     public void clearUserData() throws DataAccessException {
--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -1,22 +1,55 @@
 package dataaccess;
 
 import model.UserData;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class UserDAOTests {
 
+    private static UserDAO userDAO;
+
+    public UserDAOTests() throws DataAccessException{
+        userDAO = new SQLUserDAO();
+    }
+    @AfterEach
+    public void clearDB() throws DataAccessException{
+        userDAO.clearUserData();
+    }
+
     @Test
     public void testClearUserData() throws DataAccessException {
-        UserDAO userDAO = new SQLUserDAO();
         userDAO.clearUserData();
     }
 
     @Test
     public void addUserSuccess() throws DataAccessException {
-        UserDAO userDAO = new SQLUserDAO();
         UserData user = new UserData("bob", "123", "bob@gmail.com");
         userDAO.createUser(user);
-        userDAO.clearUserData();
+        UserData userObserved = userDAO.getUser("bob");
+        assert(userObserved.username().equals(user.username()));
     }
 
+    @Test
+    public void addUserAlreadyExists() throws DataAccessException {
+        UserData user = new UserData("bob", "123", "bob@gmail.com");
+        userDAO.createUser(user);
+        UserData user2 =new UserData("bob", "431", "bob@gmail.com");
+        Assertions.assertThrows(DataAccessException.class, () -> userDAO.createUser(user2));
+    }
+
+    @Test
+    public void getUserSuccess() throws DataAccessException {
+        UserData user = new UserData("bob", "123", "bob@gmail.com");
+        userDAO.createUser(user);
+        UserData user2 = new UserData("jill", "145145", "jill@gmail.com");
+        userDAO.createUser(user2);
+        UserData userObserved = userDAO.getUser("bob");
+        assert(userObserved.username().equals(user.username()));
+    }
+
+    @Test
+    public void getUserWithNull(){
+        Assertions.assertThrows(DataAccessException.class, () -> userDAO.getUser(null));
+    }
 }
